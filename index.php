c3f1a2957abf8df3ed8c5dfb73e0b48d6a5a7923
AyushmanChakraborty <89172555+AyushmanChakraborty@users.noreply.github.com>
2021-12-01 10:01:51 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>The Magic of Mr.Millard</title>
+</head>
+<h1>My First Heading</h>
+<p> This is the paragraph </p>
+<body>
+<script src="myscripts.js"></script>
+</body>
+</html>
